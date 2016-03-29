af09d546b1392ffca8e13fc76de20bae4af9bbda
amritbhanu <amritbhanu@gmail.com>
2016-03-29 15:10:24 -0400

    gui

--- GUI/Gui.py
+++ GUI/Gui.py
@@ -1,7 +1,10 @@
+from __future__ import print_function, division
+
 __author__ = 'amrit'
 
 from Tkinter import *
 import os
+from func_GUI import *
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 Mails_directory="/../Mails/struct.txt"
@@ -14,7 +17,9 @@ def readfile(filename):
     with open(filename,'r') as f:
         for doc in f.readlines():
             try:
-                corpus.append(doc.split(" >>> ")[1])
+                x=Email(doc.split(" >>> ")[1])
+                x.set_label(doc.split(" >>> ")[0].split()[1])
+                corpus.append(x)
                 labels.append(doc.split(" >>> ")[0].split())
             except:
                 pass
@@ -33,9 +38,9 @@ class Application(Frame):
         for i in range(len(list_mails)):
             if "inbox" in list_labels_mails[i]:
                 if "unread" in list_labels_mails[i]:
-                    self.unread.insert('end', "%0.4d : %s" % (i, list_mails[i]))
+                    self.unread.insert('end', "%0.4d : %s" % (i, list_mails[i].get_body()))
                 if "read" in list_labels_mails[i]:
-                    self.read.insert('end', "%0.4d : %s" % (i, list_mails[i]))
+                    self.read.insert('end', "%0.4d : %s" % (i, list_mails[i].get_body()))
 
     def spam_command(self):
         self.unread.delete(first=0, last=self.unread.size())
@@ -43,9 +48,9 @@ class Application(Frame):
         for i in range(len(list_mails)):
             if "spam" in list_labels_mails[i]:
                 if "unread" in list_labels_mails[i]:
-                    self.unread.insert('end', "%0.4d : %s" % (i, list_mails[i]))
+                    self.unread.insert('end', "%0.4d : %s" % (i, list_mails[i].get_body()))
                 if "read" in list_labels_mails[i]:
-                    self.read.insert('end', "%0.4d : %s" % (i, list_mails[i]))
+                    self.read.insert('end', "%0.4d : %s" % (i, list_mails[i].get_body()))
 
     def trash_command(self):
         self.unread.delete(first=0, last=self.unread.size())
@@ -53,9 +58,9 @@ class Application(Frame):
         for i in range(len(list_mails)):
             if "trash" in list_labels_mails[i]:
                 if "unread" in list_labels_mails[i]:
-                    self.unread.insert('end', "%0.4d : %s" % (i, list_mails[i]))
+                    self.unread.insert('end', "%0.4d : %s" % (i, list_mails[i].get_body()))
                 if "read" in list_labels_mails[i]:
-                    self.read.insert('end', "%0.4d : %s" % (i, list_mails[i]))
+                    self.read.insert('end', "%0.4d : %s" % (i, list_mails[i].get_body()))
 
     def del_un(self):
         x= int(self.unread.get(ANCHOR).split(' : ')[0])
@@ -98,7 +103,7 @@ class Application(Frame):
             self.trash_command()
 
     def mov_command(self):
-        print "Move"
+        print("Move")
 
     def c_labels_command(self):
         self.popup()
@@ -177,7 +182,7 @@ class Application(Frame):
         self.buttons=[]
         self.pack()
         self.createWidgets()
-        #print(Application.__getitem__(self, Button))
+        #print(Application.__getitem__(self, Button)
 
 class popupWindow1(object):
     def __init__(self,master,data):
@@ -203,10 +208,152 @@ class popupWindow(object):
         self.value=self.e.get()
         self.top.destroy()
 
-readfile(BASE_DIR+Mails_directory)
-root = Tk()
-root.title("Mailbox")
-root.minsize(width=1000,height=600 )
-app = Application(master=root)
-app.mainloop()
-root.destroy()
+"format email, str to csr"
+def format(email):
+    global hashemail
+    x=vsm(email)
+    x=hashemail(x)
+    x=l2normalize(x)
+    return x
+
+
+
+class Folder(object):
+    def __init__(self,thres=0.3):
+        self.names=['uncertain','trash']
+        self.classifier=[]
+        self.thres=thres
+        self.num=0
+
+    def addfolder(self,name):
+        self.names.append(name)
+        self.num=self.num+1
+
+    "call this to train the classifier"
+    def train(self,training_set):
+        labels=[]
+        bodies=[]
+        for mail in training_set:
+            labels.append(mail.label)
+            tmp=mail.mat
+            if bodies:
+                csr_vstack(bodies,tmp)
+            else:
+                bodies=tmp
+        self.classifier=do_SVM(labels,bodies)
+
+    "return the list of folder names the email would belong to, also will change mail.folder"
+    def predict(self,mail):
+        proba=self.classifier.predict_proba(mail.mat)
+        mail.folder=[]
+        for ind,label in enumerate(self.classifier.classes_):
+            mail.proba[label]=proba[ind]
+            if proba[ind]>self.thres:
+                mail.folder.append(label)
+        if not mail.folder:
+            mail.folder=['uncertain']
+        return mail.folder
+
+"Global variables, need to be initialized"
+feature_number=4000
+hashemail=hasher(feature_number)
+my_folder=Folder()
+pool=[]
+prog=10
+step=10
+saturation=100
+
+
+class Email(object):
+    def __init__(self,body=""):
+        self.body=body
+        if body:
+            self.mat=format(body)
+        else:
+            self.mat=[]
+        self.folder=[]
+        self.label=''
+        self.credit=0
+        self.proba={}
+        self.read=False
+
+    def set_read(self):
+        self.read=True
+
+    def set_label(self,label):
+        self.label=label
+
+    def set_body(self,body):
+        self.body=body
+        self.mat=format(body)
+
+    def set_credit(self,credit):
+        self.credit=credit
+
+    def set_folder(self,foldername):
+        self.folder=[foldername]
+
+    def get_body(self):
+        return self.body
+
+    def get_probab(self):
+        return self.proba
+
+    def get_label(self):
+        return self.label
+
+    def get_credit(self):
+        return self.credit
+
+
+
+"If user read, forward, reply this email. Will not trigger if current_folder is uncertain or trash"
+def activity_yes(email,current_folder):
+    global pool
+    email.set_label(current_folder)
+    email.set_credit(1-email.proba[current_folder])
+    if email not in pool:
+        pool.append(email)
+
+"If user move this email into a target_folder. Will not trigger if target_folder is uncertain or trash."
+'''def activity_no(email,target_folder):
+    global pool
+    email.set_label(target_folder)
+    email.set_credit(1-email.proba[current_folder])
+    if email not in pool:
+        pool.append(email)'''
+
+"Runs every mid-night? Retrain the classifier or not"
+def check_credit():
+    global pool,prog,step,my_folder,saturation
+    if len(pool) >= my_folder.num*prog*step:
+        can=[]
+        for label in my_folder.classifier.classes_:
+            collect=[x for x in pool if x.label==label]
+            credits=[x.credit for x in collect]
+            can.extend(list(np.array(collect)[np.argsort(credits)[-prog*step:]]))
+        my_folder.train(can)
+        if prog<=saturation:
+            prog=prog+1
+        else:
+            pool=can
+        return True
+    else:
+        return False
+
+if __name__ == '__main__':
+    text='abc hfsb hasb bsh'
+    email= Email(text)
+    readfile(BASE_DIR+Mails_directory)
+    global list_labels_mails
+    global list_mails
+    list_mails.append(email)
+    list_labels_mails.append(['unread', 'inbox'])
+    global pool
+    pool=list_mails
+    root = Tk()
+    root.title("Mailbox")
+    root.minsize(width=1000,height=600 )
+    app = Application(master=root)
+    app.mainloop()
+    root.destroy()
