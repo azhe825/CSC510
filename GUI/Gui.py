0e9e9acfe89dc9d1679dc57926034c3c40cebdb4
amritbhanu <amritbhanu@gmail.com>
2016-03-31 16:30:37 -0400

    update

--- GUI/Gui.py
+++ GUI/Gui.py
@@ -11,6 +11,7 @@ Mails_directory="/../Mails/struct.txt"
 
 list_mails=[]
 list_labels_mails=[]
+labels_gui=['inbox', 'uncertain','trash']
 def readfile(filename):
     corpus=[]
     labels=[]
@@ -19,6 +20,8 @@ def readfile(filename):
             try:
                 x=Email(doc.split(" >>> ")[1])
                 x.set_label(doc.split(" >>> ")[0].split()[1])
+                x.credit=1
+                x.folder.append(doc.split(" >>> ")[0].split()[1])
                 corpus.append(x)
                 labels.append(doc.split(" >>> ")[0].split())
             except:
@@ -32,35 +35,16 @@ def readfile(filename):
 
 class Application(Frame):
 
-    def inbox_command(self):
+    def button_command(self, a):
         self.unread.delete(first=0, last=self.unread.size())
         self.read.delete(first=0, last=self.read.size())
         for i in range(len(list_mails)):
-            if "inbox" in list_labels_mails[i]:
+            if a in list_labels_mails[i]:
                 if "unread" in list_labels_mails[i]:
                     self.unread.insert('end', "%0.4d : %s" % (i, list_mails[i].get_body()))
                 if "read" in list_labels_mails[i]:
                     self.read.insert('end', "%0.4d : %s" % (i, list_mails[i].get_body()))
 
-    def spam_command(self):
-        self.unread.delete(first=0, last=self.unread.size())
-        self.read.delete(first=0, last=self.read.size())
-        for i in range(len(list_mails)):
-            if "spam" in list_labels_mails[i]:
-                if "unread" in list_labels_mails[i]:
-                    self.unread.insert('end', "%0.4d : %s" % (i, list_mails[i].get_body()))
-                if "read" in list_labels_mails[i]:
-                    self.read.insert('end', "%0.4d : %s" % (i, list_mails[i].get_body()))
-
-    def trash_command(self):
-        self.unread.delete(first=0, last=self.unread.size())
-        self.read.delete(first=0, last=self.read.size())
-        for i in range(len(list_mails)):
-            if "trash" in list_labels_mails[i]:
-                if "unread" in list_labels_mails[i]:
-                    self.unread.insert('end', "%0.4d : %s" % (i, list_mails[i].get_body()))
-                if "read" in list_labels_mails[i]:
-                    self.read.insert('end', "%0.4d : %s" % (i, list_mails[i].get_body()))
 
     def del_un(self):
         x= int(self.unread.get(ANCHOR).split(' : ')[0])
@@ -70,7 +54,7 @@ class Application(Frame):
                 list_labels_mails[x][i] = ""
             if k == "inbox":
                 list_labels_mails[x][i] = "trash"
-            if k == "spam":
+            if k == "uncertain":
                 list_labels_mails[x][i] = "trash"
 
     def del_re(self):
@@ -81,12 +65,14 @@ class Application(Frame):
                 list_labels_mails[x][i] = ""
             if k == "inbox":
                 list_labels_mails[x][i] = "trash"
-            if k == "spam":
+            if k == "uncertain":
                 list_labels_mails[x][i] = "trash"
 
     def read_user(self, event):
         w = event.widget
         self.popup1(w.get(w.curselection()).split(' : ')[1])
+        global pool
+        activity_yes(pool[int(w.get(w.curselection()).split(' : ')[0])], list_labels_mails[int(w.get(w.curselection()).split(' : ')[0])][1])
 
     def unread_user(self,event):
         w = event.widget
@@ -96,31 +82,41 @@ class Application(Frame):
             if k == "unread":
                 list_labels_mails[x][i] = "read"
         if "inbox" in list_labels_mails[x]:
-            self.inbox_command()
-        if "spam" in list_labels_mails[x]:
-            self.spam_command()
+            self.button_command('inbox')
+        if "uncertain" in list_labels_mails[x]:
+            self.button_command('uncertain')
         if "trash" in list_labels_mails[x]:
-            self.trash_command()
+            self.button_command('trash')
 
-    def mov_command(self):
+    def mov_command(self,i):
+        print(i)
+        prev_label = pool[int(self.waste)].get_label()
+        e = pool[int(self.waste)]
+        e.folder= []
+        e.folder.append(i)
+        list_labels_mails[int(self.waste)]=[list_labels_mails[int(self.waste)][0],i]
+        activity_no(e,prev_label,i)
+        pool[int(self.waste)].set_label(i)
         print("Move")
 
     def c_labels_command(self):
         self.popup()
         if (self.entryValue()!=''):
-            self.buttons.append(self.entryValue())
-            x1=Button(self.m, text = self.buttons[self.count], fg   = "red", command =  self.spam_command, state=ACTIVE)
+            a=self.entryValue()
+            self.buttons.append(a)
+            labels_gui.append(a)
+            x1=Button(self.m, text = self.buttons[self.count], fg   = "red", command =  lambda: self.button_command(a), state=ACTIVE)
             self.count+=1
             self.m.add(x1)
 
     def createWidgets(self):
         self.m = PanedWindow(self, orient=VERTICAL)
         self.m.pack(side=LEFT, expand=1)
-        self.INBOX = Button(self.m, text = "INBOX", fg   = "red", command =  self.inbox_command, state=ACTIVE)
-        self.SPAM = Button(self.m, text = "SPAM", fg   = "red", command =  self.spam_command, state=ACTIVE)
-        self.TRASH = Button(self.m, text = "TRASH", fg   = "red", command =  self.trash_command, state=ACTIVE)
+        self.INBOX = Button(self.m, text = "INBOX", fg   = "red", command =  lambda: self.button_command('inbox'), state=ACTIVE)
+        self.UNCERTAIN = Button(self.m, text = "UNCERTAIN", fg   = "red", command =  lambda: self.button_command('uncertain'), state=ACTIVE)
+        self.TRASH = Button(self.m, text = "TRASH", fg   = "red", command =  lambda: self.button_command('trash'), state=ACTIVE)
         self.m.add(self.INBOX)
-        self.m.add(self.SPAM)
+        self.m.add(self.UNCERTAIN)
         self.m.add(self.TRASH)
         #self.USER.pack(side=BOTTOM, anchor=W, fill=X)
         self.m2 = PanedWindow(self, orient=VERTICAL)
@@ -137,6 +133,7 @@ class Application(Frame):
         self.unread.config(yscrollcommand = self.s.set)
         self.unread.insert('end', "Unread Mails")
         self.unread.bind("<Double-Button-1>", self.unread_user)
+        self.unread.bind("<Button-3>", self.popup_menu)
         self.unread.pack()
         self.m2.add(self.unread)
 
@@ -149,6 +146,7 @@ class Application(Frame):
         self.read.config(yscrollcommand = self.s.set)
         self.read.insert('end', "Read Mails")
         self.read.bind("<Double-Button-1>", self.read_user)
+        self.read.bind("<Button-3>", self.popup_menu)
 
         self.read.pack()
         self.m2.add(self.read)
@@ -163,6 +161,17 @@ class Application(Frame):
         self.m3.add(self.delete_re)
         self.m3.add(self.mov)
         self.m3.add(self.CREATE_Labels)
+
+        self.aMenu = Menu(self, tearoff=0)
+        for i in labels_gui:
+            self.aMenu.add_command(label=i.upper(), command=lambda: self.mov_command(i.lower()))
+
+    def popup_menu(self, event):
+        w = event.widget
+        self.waste=w.get(w.curselection()).split(' : ')[0]
+        #self.popup1(w.get(w.curselection()).split(' : ')[1])
+        #x = int(w.get(w.curselection()).split(' : ')[0])
+        self.aMenu.post(event.x_root, event.y_root)
 
     def popup(self):
         self.w=popupWindow(self.Frame)
@@ -181,6 +190,7 @@ class Application(Frame):
         self.count=0
         self.buttons=[]
         self.pack()
+        self.waste=''
         self.createWidgets()
         #print(Application.__getitem__(self, Button)
 
@@ -316,12 +326,12 @@ def activity_yes(email,current_folder):
         pool.append(email)
 
 "If user move this email into a target_folder. Will not trigger if target_folder is uncertain or trash."
-'''def activity_no(email,target_folder):
+def activity_no(email,target_folder, current_folder):
     global pool
     email.set_label(target_folder)
     email.set_credit(1-email.proba[current_folder])
     if email not in pool:
-        pool.append(email)'''
+        pool.append(email)
 
 "Runs every mid-night? Retrain the classifier or not"
 def check_credit():
@@ -342,15 +352,20 @@ def check_credit():
         return False
 
 if __name__ == '__main__':
+    # An incoming mail
     text='abc hfsb hasb bsh'
     email= Email(text)
+    email.credit=1
+    email.label='inbox'
+    email.folder.append('inbox')
+
+    #pool of mails with intial labels.
     readfile(BASE_DIR+Mails_directory)
-    global list_labels_mails
-    global list_mails
     list_mails.append(email)
     list_labels_mails.append(['unread', 'inbox'])
-    global pool
     pool=list_mails
+    #check_credit()
+    # GUI
     root = Tk()
     root.title("Mailbox")
     root.minsize(width=1000,height=600 )
