d900cb6c41be466a4c2cbcd41294ed2eaae45632
sli41 <sli41@ncsu.edu>
2016-04-06 11:11:07 -0400

    codes to check updated new mails

--- EmailGeneratorAndWebUI/updateEmails.py
+++ EmailGeneratorAndWebUI/updateEmails.py
@@ -19,22 +19,29 @@ def fetchMail(mailbox):
     ids = emailIndex[0]  # emailIndex is a list.
     id_list = ids.split()  # ids is a space separated string
     for id in id_list:
-        result, data = mailbox.fetch(id, "(RFC822)") # fetch the email body (RFC822) for the given ID
+        result, mailtext = mailbox.fetch(id, "(RFC822)") # fetch the email body (RFC822) for the given ID
         if result == 'OK':
-            mailList.append(data[0][1])
+            mailList.append(mailtext[0][1])
         else:
             print "Email not available!"
-    return mailList
+    return id_list, mailList
 
 def main():
     loginID = 'test2016emailreceiver@gmail.com'
     password = 'password2016END'
+    record_id_list = ['1']
     status, mailPort = loginMail(loginID, password)
     print status
     mailList = list()
     if status[0] == 'OK':
-        mailList = fetchMail(mailPort)
-    for mail in mailList:
+        id_list, mailList = fetchMail(mailPort)
+    # check for new mail update
+    numberOfNewMail = len(id_list) - len(record_id_list)
+    newMails = list()
+    if numberOfNewMail != 0:
+        for i in range(numberOfNewMail):
+            newMails.append(mailList[-(i+1)])
+    for mail in newMails:
         print mail
 
 if __name__ == "__main__":
