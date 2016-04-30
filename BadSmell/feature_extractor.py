13529d5c4064523b5c4d6b4120b9e7ba7f6a173a
Di Chen <dchen20@ncsu.edu>
2016-04-30 15:05:39 -0400

    (1) Issues Durations

--- BadSmell/feature_extractor.py
+++ BadSmell/feature_extractor.py
@@ -8,8 +8,9 @@ def extract_feature():
     events = [event for event in os.listdir(csvpath) if event.__contains__('event')]
     comments = [comment for comment in os.listdir(csvpath) if comment.__contains__('comment')]
     group_features1 = process_event(csvpath, events)
-    generate_assignees_csv(base, group_features1)
     group_features2 = process_comment(csvpath, comments)
+    generate_assignees_csv(base, group_features1)
+    generate_issueDuration_csv(base, group_features1)
     generate_userCommentNum_csv(base, group_features2)
     generate_issueCommentNum_csv(base, group_features2)
     generate_issueParticipants_csv(base, group_features2)
@@ -56,10 +57,18 @@ def process_event(csvpath, events):
     group_features = {}
     for group in events:
         with open(os.path.join(csvpath, group), 'r') as csvinput:
+            issue_times = {}
             user_assigned = {}
             reader = csv.DictReader(csvinput)
             ## begin extract information from each row, i.e. event.
             for row in reader:
+                ## (1) Long Open Issues
+                issueID = row['issueID']
+                if not issue_times.get(issueID):
+                    issue_times[issueID] = [row['time']]
+                else:
+                    issue_times[issueID].append(row['time'])
+
                 ## (4)  Equal Number of Issue Assignees
                 if row['action'] == 'assigned':
                     user = (row['user'].split('/'))[1]
@@ -71,8 +80,18 @@ def process_event(csvpath, events):
         if not group_features.get(groupID):
             group_features[groupID] = {}
         group_features[groupID]['Issue Assignees'] = user_assigned
+        group_features[groupID]['Issue Times'] = issue_times
+        group_features[groupID]['Issue Duration'] = {k: get_duration(v) for k,v in issue_times.iteritems()}
         print  group + ' finished'
     return group_features
+
+
+def get_duration(times):
+    "return time in days"
+    if len(times) <= 1:
+        return 0
+    else:
+        return (int(max(times))-int(min(times))) / (24*3600)
 
 
 def generate_assignees_csv(base, group_features):
@@ -100,6 +119,21 @@ def generate_userCommentNum_csv(base, group_features):
             input_data.update(dict)
             fileds = ['groupID']
             fileds.extend(users)
+            writer = csv.DictWriter(csvinput, fieldnames=fileds)
+            writer.writeheader()
+            writer.writerow(input_data)
+
+
+def generate_issueDuration_csv(base, group_features):
+    result_file = csvpath = os.path.join(base, 'featureCSV/issueDuration.csv')
+    with open(result_file, 'w') as csvinput:
+        for groupID, feature in group_features.iteritems():
+            dict = feature['Issue Duration']
+            issues = dict.keys()
+            input_data = {'groupID': groupID}
+            input_data.update(dict)
+            fileds = ['groupID']
+            fileds.extend(issues)
             writer = csv.DictWriter(csvinput, fieldnames=fileds)
             writer.writeheader()
             writer.writerow(input_data)
